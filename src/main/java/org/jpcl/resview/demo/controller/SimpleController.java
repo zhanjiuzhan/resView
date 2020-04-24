de8b72f06c78e72ea9175c276608c3afe7a813cf
chenglei <chenglei@4366.com>
2020-04-24 16:21:54 +0800

    日志添加

--- src/main/java/org/jpcl/resview/demo/controller/SimpleController.java
+++ src/main/java/org/jpcl/resview/demo/controller/SimpleController.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSON;
 import org.jpcl.resview.demo.model.User;
 import org.jpcl.resview.view.JcResView;
 import org.jpcl.resview.view.model.Status;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,8 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/sample")
 public class SimpleController {
 
+    Logger logger = LoggerFactory.getLogger(SimpleController.class);
     @RequestMapping("/getUser")
     public JcResView getUser() {
+        logger.info("getUser {} {}", "aa", "s");
         JcResView jc = new JcResView();
         User user = new User();
         user.setAge(11);
